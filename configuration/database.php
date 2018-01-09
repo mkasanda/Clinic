2f1609937984aab9c2780dbe89215f10928dbf3d
Malama Kasanda <malamakaka@gmail.com>
2018-01-09 14:57:36 +0100

    Update database.php

--- configuration/database.php
+++ configuration/database.php
@@ -1,1 +1,12 @@
+<?php
+$host="localhost"; // Host name
+$username="root"; // Mysql username
+$password="malama"; // Mysql password
+$db_name="Unza_Clinic_Prototype"; // Database name
 
+//Connect to server and select database
+$connection = @mysql_connect($host, $username, $password) or die(mysql_error());
+
+$db = @mysql_select_db($db_name, $connection) or die(mysql_error());
+
+?>
